bbdc50cd034b1ae56e3c2aa54cedc1a30911806c
Andreas Eriksson <ae225ak@student.lnu.se>
2026-03-10 10:57:51 +0100

    Add OpenAPI support

--- src/main/java/me/andreaseriksson/ufoapi/WebapiApplication.java
+++ src/main/java/me/andreaseriksson/ufoapi/WebapiApplication.java
@@ -1,9 +1,16 @@
 package me.andreaseriksson.ufoapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+		title = "UFO Sightings API",
+		version = "1.0",
+		description = "API for querying UFO sighting data"
+))
 public class WebapiApplication {
 
 	public static void main(String[] args) {
